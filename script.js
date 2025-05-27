599dabd8054a6de258768a785c781e071f7f3290
GoldPapaya <lucasdcrawford32@gmail.com>
2025-05-27 16:21:18 -0400

    fix: added scroll animation to other #contact references

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('active');
+  document.querySelector('.nav-links').classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a, .hero .btn, .about p a[href="#contact"]').forEach(link => {
+  link.addEventListener('click', (event) => {
+    document.querySelector('.nav-links').classList.remove('active');
+    const targetId = link.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      event.preventDefault();
+      const headerHeight = document.querySelector('header').offsetHeight;
+      const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight - 10;
+      window.scrollTo({ top: targetPosition, behavior: 'smooth' });
+    }
   });
-  
-  document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', (event) => {
-      document.querySelector('.nav-links').classList.remove('active');
-      const targetId = link.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      if (targetElement) {
-        event.preventDefault();
-        const headerHeight = document.querySelector('header').offsetHeight;
-        const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight - 10; // 10px buffer override
-        window.scrollTo({
-          top: targetPosition,
-          behavior: 'smooth'
-        });
-      }
-    });
-  });
+});
